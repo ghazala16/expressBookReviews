ea355fa258a03afff4314ace6a1171387d1d229a
ghazala16 <ghazalajamal16@gmail.com>
2025-07-21 11:46:25 -0400

    final changes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -14,24 +15,22 @@ public_users.post("/register", (req, res) => {
         return res.status(400).json({ message: "Username and password are required" });
     }
 
-    // Check if user already exists
     if (users.some(user => user.username === username)) {
         return res.status(409).json({ message: "User already exists" });
     }
 
-    // Add new user
     users.push({ username, password });
     return res.status(201).json({ message: "User registered successfully" });
 });
 
 
 // Task 1: Get the book list available in the shop
-public_users.get('/', function (req, res) {
+public_users.get('/', (req, res) => {
     return res.status(200).send(JSON.stringify(books, null, 4));
 });
 
 // Task 2: Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
+public_users.get('/isbn/:isbn', (req, res) => {
     const isbn = req.params.isbn;
     const book = books[isbn];
 
@@ -43,23 +42,21 @@ public_users.get('/isbn/:isbn', function (req, res) {
 });
 
 // Task 3: Get book details based on author
-public_users.get('/author/:author', function (req, res) {
+public_users.get('/author/:author', (req, res) => {
     const author = req.params.author;
     const filteredBooks = Object.values(books).filter(book => book.author === author);
-
     return res.status(200).send(filteredBooks);
 });
 
 // Task 4: Get all books based on title
-public_users.get('/title/:title', function (req, res) {
+public_users.get('/title/:title', (req, res) => {
     const title = req.params.title;
     const filteredBooks = Object.values(books).filter(book => book.title === title);
-
     return res.status(200).send(filteredBooks);
 });
 
 // Task 5: Get book review
-public_users.get('/review/:isbn', function (req, res) {
+public_users.get('/review/:isbn', (req, res) => {
     const isbn = req.params.isbn;
     const book = books[isbn];
 
@@ -69,5 +66,52 @@ public_users.get('/review/:isbn', function (req, res) {
         return res.status(404).json({ message: "Book not found" });
     }
 });
+
+
+/* ------------------- Promises / Async-Await for Tasks 10-13 ------------------- */
+
+// Task 10: Get all books using async/await
+public_users.get('/async-books', async (req, res) => {
+    try {
+        const response = await axios.get('http://localhost:5000/');
+        return res.status(200).send(response.data);
+    } catch (err) {
+        return res.status(500).json({ message: "Error fetching books" });
+    }
+});
+
+// Task 11: Get book details by ISBN using async/await
+public_users.get('/async-isbn/:isbn', async (req, res) => {
+    const isbn = req.params.isbn;
+    try {
+        const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+        return res.status(200).send(response.data);
+    } catch (err) {
+        return res.status(404).json({ message: "Book not found with this ISBN" });
+    }
+});
+
+// Task 12: Get books by author using async/await
+public_users.get('/async-author/:author', async (req, res) => {
+    const author = req.params.author;
+    try {
+        const response = await axios.get(`http://localhost:5000/author/${author}`);
+        return res.status(200).send(response.data);
+    } catch (err) {
+        return res.status(404).json({ message: "Books not found for this author" });
+    }
+});
+
+// Task 13: Get books by title using async/await
+public_users.get('/async-title/:title', async (req, res) => {
+    const title = req.params.title;
+    try {
+        const response = await axios.get(`http://localhost:5000/title/${title}`);
+        return res.status(200).send(response.data);
+    } catch (err) {
+        return res.status(404).json({ message: "Books not found for this title" });
+    }
+});
+
 
 module.exports.general = public_users;
